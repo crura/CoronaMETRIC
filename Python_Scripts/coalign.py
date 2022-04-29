6edcebf64eed8fd9c2ca2ff327da788895922faa
crura <rura@cua.edu>
2022-04-29 15:42:45 -0400

    initialize python script for image coalignment

---
+++ Python_Scripts/coalign.py
@@ -0,0 +1,28 @@
+#!'/Users/crura/.conda/envs/test_env/lib/python3.9'
+import sunpy
+import matplotlib.pyplot as plt
+import sunpy.version
+import sunpy.map
+import matplotlib.colors
+import astropy.units as u
+from astropy.io import fits
+
+fits_dir = "/Users/crura/Desktop/Research/Magnetic_Field/Forward_MLSO_Projection.fits"
+data = fits.getdata(fits_dir)
+head = fits.getheader(fits_dir)
+
+psimap = sunpy.map.Map(data, head)
+
+fits_dir_mlso = '/Users/crura/Desktop/Research/Magnetic_Field/Coalign/20170829_200801_kcor_l2_avg.fts'
+
+data = fits.getdata(fits_dir_mlso)
+head = fits.getheader(fits_dir_mlso)
+head['detector'] = ('KCor')
+mlsomap = sunpy.map.Map(data, head)
+
+fig = plt.figure(figsize=(12, 5))
+ax1 = fig.add_subplot(1, 2, 1, projection=psimap)
+psimap.plot(axes=ax1,norm=matplotlib.colors.LogNorm())
+ax2 = fig.add_subplot(1, 2, 2, projection=mlsomap)
+mlsomap.plot(axes=ax2)
+plt.show()

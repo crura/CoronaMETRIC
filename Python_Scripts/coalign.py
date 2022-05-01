7d041687181283f24cd756f3ce3d36c1b0d310cf
crura <rura@cua.edu>
2022-05-01 14:49:13 -0400

    add python script to generate plot of coaligned images

--- Python_Scripts/coalign.py
+++ Python_Scripts/coalign.py
@@ -7,6 +7,7 @@ import matplotlib.colors
 import astropy.units as u
 from astropy.io import fits
 from pathlib import Path
+import pandas as pd
 
 path = __file__
 pathnew = Path(path)
@@ -34,4 +35,38 @@ ax1 = fig.add_subplot(1, 2, 1, projection=psimap)
 psimap.plot(axes=ax1,norm=matplotlib.colors.LogNorm())
 ax2 = fig.add_subplot(1, 2, 2, projection=mlsomap)
 mlsomap.plot(axes=ax2)
+plt.show()
+
+
+
+# Import Coaligned Images
+psi_mlso_proj_pb =pd.read_csv('/Users/crura/Desktop/Research/Magnetic_Field/FORWARD_MLSO_Rotated_Data/PSI_PB_MLSO_Coalignment.csv', sep=',',header=None)
+
+
+psi_mlso_proj_pb_values = psi_mlso_proj_pb.values
+
+
+fits_dir_mlso = '/Users/crura/Desktop/Research/Magnetic_Field/Coalign/20170829_200801_kcor_l2_avg.fts'
+
+# add KCor as detector to fits file header
+from astropy.io import fits
+data = psi_mlso_proj_pb_values
+head = fits.getheader(fits_dir_mlso)
+#mlsomap.fits_header.insert('Detector', 'kcor')
+head['Observatory'] = ('PSI-MAS')
+head['detector'] = ('KCor')
+
+psi_coalign_map_pb = sunpy.map.Map(data, head)
+#psi_coalign_map_pb
+
+
+fig = plt.figure(figsize=(12, 5))
+ax1 = fig.add_subplot(1, 2, 1, projection=psi_coalign_map_pb)
+ax1.set_title('PSI-Forward Model')
+psi_coalign_map_pb.plot(axes=ax1,norm=matplotlib.colors.LogNorm())
+ax2 = fig.add_subplot(1, 2, 2, projection=mlsomap)
+mlsomap.plot(axes=ax2)
+ax2.title.set_text('MLSO K-COR Observation 2017-08-29')
+ax1.title.set_text('PSI/FORWARD Synthetic Polarized Brightness Eclipse Model')
+plt.savefig('/Users/crura/Desktop/Research/Presentations/URD_22_Pictures/Model_Comparison.png')
 plt.show()

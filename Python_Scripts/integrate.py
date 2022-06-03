20d8ff72088496d67ec226bebd52567b7e677219
crura <rura@cua.edu>
2022-06-03 01:53:13 -0400

    fix: correct path to account for new save location of model_parameters.sav

--- Python_Scripts/integrate.py
+++ Python_Scripts/integrate.py
@@ -11,7 +11,9 @@ import git
 from scipy.io import readsav
 import unittest
 
-
+repo = git.Repo('.', search_parent_directories=True)
+repo_path = repo.working_tree_dir
+idl_save = readsav(os.path.join(repo_path,'Data/model_parameters.sav'))
 idl_save = readsav('/Users/crura/model_parameters.sav')
 crln_obs = idl_save['crln_obs']
 crlt_obs = idl_save['crlt_obs']

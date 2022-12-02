bc6d6f72c2bf56cdf2ea13abe148b93b72868db9
crura <rura@cua.edu>
2022-12-02 16:50:03 -0500

    set naming scheme for output plot

--- Python_Scripts/plot.py
+++ Python_Scripts/plot.py
@@ -26,70 +26,14 @@ idl_save = readsav(os.path.join(repo_path,'Data/model_parameters.sav'))
 date_obs =idl_save['DATE_OBS']
 crln_obs_print = idl_save['crln_obs_print']
 crlt_obs_print = idl_save['crlt_obs_print']
-fits_directory = idl_save['fits_directory']
+fits_directory = str(idl_save['fits_directory'][0],'utf-8')
 occlt = idl_save['occlt']
 shape = idl_save['shape']
 params = str(crlt_obs_print,'utf-8') + '_' +  str(crln_obs_print,'utf-8')
 
-fits_dir = pathnew.parent.parent.joinpath('Data/PSI/Forward_MLSO_Projection.fits')
-path_fits_dir = Path(fits_dir)
 
 
-data = fits.getdata(fits_dir)
-head = fits.getheader(fits_dir)
-
-psimap = sunpy.map.Map(data, head)
-
-fits_dir_mlso = pathnew.parent.parent.joinpath(str(fits_directory[0],encoding='utf-8'))
-
-data = fits.getdata(fits_dir_mlso)
-head = fits.getheader(fits_dir_mlso)
-head['detector'] = ('KCor')
-mlsomap = sunpy.map.Map(data, head)
-
-psimap.plot_settings['norm'] = plt.Normalize(mlsomap.min(), mlsomap.max())
-#
-# fig = plt.figure(figsize=(12, 5))
-# ax1 = fig.add_subplot(1, 2, 1, projection=psimap)
-# psimap.plot(axes=ax1,norm=matplotlib.colors.LogNorm())
-# ax2 = fig.add_subplot(1, 2, 2, projection=mlsomap)
-# mlsomap.plot(axes=ax2)
-# plt.show()
-#
-#
-#
-# # Import Coaligned Integrated Electron Density
-# integrated_dens_coaligned =pd.read_csv(pathnew.parent.parent.joinpath('Output/FORWARD_MLSO_Rotated_Data/PSI_MLSO_Integrated_Electron_Density_Coalignment.csv'), sep=',',header=None)
-# integrated_dens_coaligned_values = integrated_dens_coaligned.values
-# print(integrated_dens_coaligned_values.shape)
-#
-# # add KCor as detector to fits file header
-# from astropy.io import fits
-# data = integrated_dens_coaligned_values
-# head = fits.getheader(fits_dir_mlso)
-# #mlsomap.fits_header.insert('Detector', 'kcor')
-# head['Observatory'] = ('PSI-MAS')
-# head['detector'] = ('Cor-1')
-#
-# psi_coalign_map_pb = sunpy.map.Map(data, head)
-# #psi_coalign_map_pb
-#
-#
-# fig = plt.figure(figsize=(12, 5))
-# ax1 = fig.add_subplot(1, 2, 1, projection=psi_coalign_map_pb)
-# ax1.set_title('PSI-Forward Model')
-# psi_coalign_map_pb.plot(axes=ax1,norm=matplotlib.colors.LogNorm())
-# ax2 = fig.add_subplot(1, 2, 2, projection=mlsomap)
-# mlsomap.plot(axes=ax2)
-# ax2.title.set_text('MLSO K-COR Observation {}'.format(str(date_obs,'utf-8') + 'Z'))
-# ax1.title.set_text('PSI/FORWARD Polarized Brightness Eclipse Model ' + params)
-# # plt.savefig(pathnew.parent.parent.joinpath('Data/Output/FORWARD_MLSO_Rotated_Data/Plots/Model_Comparison_{}.png'.format(params)))
-# # plt.show()
-#
-#
-
-
-fits_dir_cor1 = os.path.join(repo_path,'Data/COR1/2017_08_20_rep_avg.fts')
+fits_dir_cor1 = os.path.join(fits_directory)
 
 data2 = fits.getdata(fits_dir_cor1)
 head2 = fits.getheader(fits_dir_cor1)
@@ -122,8 +66,11 @@ psimap.plot_settings['norm'] = plt.Normalize(cor1map.min(), cor1map.max())
 psimap.plot(axes=ax2,title=False,norm=matplotlib.colors.LogNorm())
 R_SUN = occlt * (head1['rsun'] / head1['cdelt1'])
 ax2.add_patch(Circle((int(shape/2),int(shape/2)), R_SUN, color='black',zorder=100))
-ax1.title.set_text('COR-1 Observation {}'.format(str(date_obs,'utf-8') + 'Z'))
-ax2.title.set_text('Corresponding PSI/FORWARD pB Eclipse Model')
-plt.savefig(os.path.join(repo_path,'Output/Plots/Model_Comparison.png'))
+ax1.set_title('COR-1 Observation {}'.format(str(date_obs,'utf-8') + 'Z'), fontsize=18)
+ax2.set_title('Corresponding PSI/FORWARD pB Eclipse Model', fontsize=18)
+
+string_print = str(date_obs,'utf-8').split('T')[0].replace('-','_') + 'cor1'
+
+plt.savefig(os.path.join(repo_path,'Output/Plots/Model_Comparison_{}.png'.format(string_print)))
 plt.show()
 plt.close()

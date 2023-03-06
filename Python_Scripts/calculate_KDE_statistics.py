e70107b136b694042c3284622b826da56659b5f9
crura <rura@cua.edu>
2023-03-06 12:31:05 -0500

    import redefined functions from functions.py, calculate and print KDE statistics

--- Python_Scripts/calculate_KDE_statistics.py
+++ Python_Scripts/calculate_KDE_statistics.py
@@ -27,8 +27,10 @@ from scipy.interpolate import interp1d
 import matplotlib
 from os.path import join, isfile
 from os import listdir
+from scipy.stats import gaussian_kde
 matplotlib.use('TkAgg')
 mpl.use('TkAgg')
+from functions import KL_div, JS_Div, calculate_KDE, calculate_KDE_statistics
 
 
 repo = git.Repo('.', search_parent_directories=True)
@@ -51,5 +53,28 @@ for i in datafiles:
     err_forward_cor1_central_new = np.concatenate([err_forward_cor1_central_new,idl_save['ERR_ARR_FORWARD']])
     err_forward_cor1_los_new = np.concatenate([err_forward_cor1_los_new,idl_save['ERR_ARR_LOS_FORWARD']])
     err_random_new = np.concatenate([err_random_new,idl_save['ERR_ARR_RND']])
-    
-    
+
+
+# convert arrays from radians to degrees
+err_cor1_central_deg_new = err_cor1_central_new[np.where(err_cor1_central_new > 0)]*180/np.pi
+err_forward_cor1_central_deg_new = err_forward_cor1_central_new[np.where(err_forward_cor1_central_new > 0)]*180/np.pi
+err_random_deg_new = err_random_new[np.where(err_random_new > 0)]*180/np.pi
+
+
+KDE_cor1_central_deg_new = calculate_KDE(err_cor1_central_deg_new)
+KDE_forward_cor1_central_deg_new = calculate_KDE(err_forward_cor1_central_deg_new)
+KDE_random_deg_new = calculate_KDE(err_random_deg_new)
+
+JSD_cor1_forward_central_new, KLD_cor1_forward_central_new = calculate_KDE_statistics(KDE_cor1_central_deg_new, KDE_forward_cor1_central_deg_new)
+JSD_cor1_central_random_new, KLD_cor1_central_random_new = calculate_KDE_statistics(KDE_cor1_central_deg_new, KDE_random_deg_new)
+JSD_COR1_Forward_Central_Random_new, KLDcor1_forward_central_random_new = calculate_KDE_statistics(KDE_forward_cor1_central_deg_new, KDE_random_deg_new)
+
+combined_dict = dict(metric=['KL Divergence', 'JS Divergence'],
+                    cor1_v_psi=[KLD_cor1_forward_central_new, JSD_cor1_forward_central_new],
+                    cor1_v_random=[KLD_cor1_central_random_new, JSD_cor1_central_random_new],
+                    psi_v_random=[KLDcor1_forward_central_random_new, JSD_COR1_Forward_Central_Random_new])
+
+pd.set_option('display.float_format', '{:.3E}'.format)
+stats_df = pd.DataFrame(combined_dict)
+stats_df.columns = ['metric', 'cor1 vs psi pB', 'cor1 vs random', 'psi pB vs random']
+print(stats_df.to_latex(index=False))

3244b5910874ba67beb9db68fbb26e3f08cff94d
crura <rura@cua.edu>
2024-05-31 11:04:10 -0400

    fix: change order of queries to print central tendency statistics before tukey hsd statistics

--- Python_Scripts/print_results.py
+++ Python_Scripts/print_results.py
@@ -55,15 +55,6 @@ print_sql_query(dbName, query, latex=True, print_to_file=True, output_file=os.pa
 # print_sql_query(dbName, query, latex=True, print_to_file=True, caption=True, caption_text='Central Tendency Statistics for K-COR Data Combined',
 #                  output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
 
-
-query = "SELECT group1, group2, date, mean_diff, p_adj, lower_bound_ci, upper_bound_ci, KLD, JSD, reject from tukey_hsd_stats_cor1 inner join central_tendency_stats_cor1_new on central_tendency_stats_cor1_new.id = tukey_hsd_stats_cor1.group_1_central_tendency_stats_cor1_id;"
-print_sql_query(dbName, query, latex=True, print_to_file=True, caption=True, caption_text='Tukey HSD Statistics for COR-1 Data',
-                 output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
-
-query = "SELECT group1, group2, date, mean_diff, p_adj, lower_bound_ci, upper_bound_ci, KLD, JSD, reject from tukey_hsd_stats_kcor inner join central_tendency_stats_kcor_new on central_tendency_stats_kcor_new.id = tukey_hsd_stats_kcor.group_1_central_tendency_stats_kcor_id;"
-print_sql_query(dbName, query, latex=True, print_to_file=True, caption=True, caption_text='Tukey HSD Statistics for K-COR Data',
-                 output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
-
 query = "SELECT data_type, date, mean, median, standard_deviation, confidence_interval, n, Gaussian_JSD, Gaussian_KLD, kurtosis, skewness from central_tendency_stats_cor1_new where date!='combined' order by mean asc;"
 print_sql_query(dbName, query, print_to_file=True, latex=True, caption=True, caption_text='Central Tendency Statistics for COR-1 Data By Date',
                  output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
@@ -80,8 +71,18 @@ query = "SELECT data_type, date, mean, median, standard_deviation, confidence_in
 print_sql_query(dbName, query, print_to_file=True, latex=True, caption=True, caption_text='Central Tendency Statistics for K-COR Data By Date',
                  output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
 
-query = "SELECT data_type, date, mean, median, standard_deviation, confidence_interval, n, Gaussian_JSD, Gaussian_KLD, kurtosis, skewness from central_tendency_stats_kcor_all where date='combined' order by mean asc;"
+query = "SELECT data_type, date, mean, median, standard_deviation, confidence_interval, n, Gaussian_JSD, Gaussian_KLD, kurtosis, skewness from central_tendency_stats_kcor_new where date='combined' order by mean asc;"
 print_sql_query(dbName, query, latex=True, print_to_file=True, caption=True, caption_text='Central Tendency Statistics for K-COR Data Combined',
+                 output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
+
+
+
+query = "SELECT group1, group2, date, mean_diff, p_adj, lower_bound_ci, upper_bound_ci, KLD, JSD, reject from tukey_hsd_stats_cor1 inner join central_tendency_stats_cor1_new on central_tendency_stats_cor1_new.id = tukey_hsd_stats_cor1.group_1_central_tendency_stats_cor1_id;"
+print_sql_query(dbName, query, latex=True, print_to_file=True, caption=True, caption_text='Tukey HSD Statistics for COR-1 Data',
+                 output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
+
+query = "SELECT group1, group2, date, mean_diff, p_adj, lower_bound_ci, upper_bound_ci, KLD, JSD, reject from tukey_hsd_stats_kcor inner join central_tendency_stats_kcor_new on central_tendency_stats_kcor_new.id = tukey_hsd_stats_kcor.group_1_central_tendency_stats_kcor_id;"
+print_sql_query(dbName, query, latex=True, print_to_file=True, caption=True, caption_text='Tukey HSD Statistics for K-COR Data',
                  output_file=os.path.join(repo_path, 'Output/Plots/Print_Out.txt'))
 
 # query = "SELECT Gaussian_JSD, Gaussian_KLD, kurtosis, skewness from central_tendency_stats_kcor_all where date!='combined' order by mean asc;"

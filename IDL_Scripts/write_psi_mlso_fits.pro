b645e6c136596c1e8c4079e8e6851b0ae04aa524
crura <rura@cua.edu>
2025-02-28 17:07:34 -0500

    apply Apache License 2.0 to repository

--- IDL_Scripts/write_psi_mlso_fits.pro
+++ IDL_Scripts/write_psi_mlso_fits.pro
@@ -1,3 +1,16 @@
+; Copyright 2025 Christopher Rura
+
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+
+;     http://www.apache.org/licenses/LICENSE-2.0
+
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
 function write_psi_mlso_fits, directory
   restore, '/Users/crura/Desktop/Research/Magnetic_Field/Forward_PB_data.sav',/v
   ;.compile -v '/Users/crura/SSW/gen/idl/util/default.pro'
--- Python_Scripts/test.py
+++ Python_Scripts/test.py
@@ -1,3 +1,16 @@
+# Copyright 2025 Christopher Rura
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from integrate import arrDens_central, params, image_sum, bx_central_image_data, by_central_image_data, bz_central_image_data, image_sum_bx, image_sum_by, image_sum_bz, arrDens_forward
 # from coalign import bz_integrated_coaligned, by_integrated_coaligned, bx_integrated_coaligned, bx_central_coaligned, by_central_coaligned, bz_central_coaligned,
 from scipy.io import readsav

9429d46110f44de7609112b75b8edbdfe7a856f8
crura <rura@cua.edu>
2025-06-10 17:11:25 -0400

    no longer run plot.py in IDL workflow but when generating all plots

--- test_run_python.sh
+++ test_run_python.sh
@@ -17,9 +17,11 @@ conda activate test_env &&
 python3 -m venv env &&
 source env/bin/activate &&
 pip install -r requirements.txt &&
-# python Python_Scripts/new_plot_paper_figures.py
+python Python_Scripts/new_plot_paper_figures.py
 # python Python_Scripts/test_new_plot_paper_figs.py
 # python Python_Scripts/calculate_KDE_statistics.py
 python Python_Scripts/Test_Plot_3_0_Features.py 
 python Python_Scripts/print_results.py 
+python Python_Scripts/Plot_Final_Paper_Figures.py
+python Python_Scripts/plot.py
 deactivate
